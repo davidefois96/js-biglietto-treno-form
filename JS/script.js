0dbc5bcb36dc7990d1599c908f414fa5166f35d9
davidefois96 <davidefois360@gmail.com>
2024-02-21 21:51:48 +0100

    [add] new code

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,6 @@ let Nome;
 const numeroChilometri = document.querySelector('.numeroChilometri');
 let NumeroChilometri;
 const etaPasseggero = document.querySelector('.etaPasseggero');
-
 const  prezzoAlChilometro = 0.21 ;
 let prezzoTotale ;
 let discount;
@@ -13,54 +12,56 @@ const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
 let ticket = document.querySelector('.ticket');
 
+
+
+
 btn1.addEventListener('click',function(event){
 
   event.preventDefault();
-
+  
   Nome= nome.value ;
+
   NumeroChilometri= numeroChilometri.value ;
-  
-  
 
+  if(isNaN(NumeroChilometri) == false && isNaN(Nome)== true){
   
-  console.log(NumeroChilometri, Nome, etaPasseggero);
+   console.log(NumeroChilometri, Nome, etaPasseggero);
 
-  if(etaPasseggero.value =='Minorenne'){
-   prezzoTotale = (NumeroChilometri*prezzoAlChilometro*0.8);
-   discount = 'hai ricevuto il 20% di sconto!';
+   if(etaPasseggero.value =='Minorenne'){
+
+     prezzoTotale = (NumeroChilometri*prezzoAlChilometro*0.8);
+     discount = 'hai ricevuto il 20% di sconto!';
+
+    }else if(etaPasseggero.value =='Over65' ){
+
+     prezzoTotale = (NumeroChilometri*prezzoAlChilometro*0.6)
+     discount = 'hai ricevuto il 40% di sconto!';
    
-  
+    }else{
 
+     prezzoTotale = (NumeroChilometri*prezzoAlChilometro);
+     discount = 'nessuno sconto applicato';
 
-  }else if(etaPasseggero.value =='Over65' ){
-   prezzoTotale = (NumeroChilometri*prezzoAlChilometro*0.6)
-   discount = 'hai ricevuto il 40% di sconto!';
-   
- 
-  
+    }
+
+    console.log(prezzoTotale);
+
+    ticket.classList.remove('d-none')
+
+    document.getElementById('output').innerHTML ='Ciao! ' + Nome + ' ecco il tuo ticket dà ' + prezzoTotale.toFixed(2) + '€ ' + discount;
+
   }else{
-   prezzoTotale = (NumeroChilometri*prezzoAlChilometro);
-   discount = 'nessuno sconto applicato';
-  
 
- 
+    alert('qualcosa è andato storto')
 
- 
   }
-
-  console.log(prezzoTotale);
-
-  ticket.classList.toggle('d-none')
-
-  
-  document.getElementById('output').innerHTML ='Ciao! ' + Nome + ' ecco il tuo ticket dà ' + prezzoTotale.toFixed(2) + '€ ' + discount;
-
-
-
-  
   
   
 })
+
+
+
+
 
 
 
