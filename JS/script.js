d75340b1f4cc81df6781f61088b723668d0da792
davidefois96 <davidefois360@gmail.com>
2024-02-21 20:02:00 +0100

    [add] new code

--- JS/script.js
+++ JS/script.js
@@ -2,22 +2,62 @@ const nome = document.querySelector('.nome');
 let Nome;
 const numeroChilometri = document.querySelector('.numeroChilometri');
 let NumeroChilometri;
-const etàPasseggero = document.querySelector('.etàPasseggero');
-let EtàPasseggero;
+const etaPasseggero = document.querySelector('.etaPasseggero');
+
 const  prezzoAlChilometro = 0.21 ;
-let prezzoTotale;
+let prezzoTotale ;
+let discount;
+const discountFirst= 0.8;
+const discountSecond= 0.6;
 const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
+let ticket = document.querySelector('.ticket');
 
 btn1.addEventListener('click',function(event){
 
   event.preventDefault();
+
   Nome= nome.value ;
   NumeroChilometri= numeroChilometri.value ;
-  Etàpasseggero= etàPasseggero.value;
+  
+  
 
   
-  console.log(NumeroChilometri, Nome, Etàpasseggero);
+  console.log(NumeroChilometri, Nome, etaPasseggero);
+
+  if(etaPasseggero.value =='Minorenne'){
+   prezzoTotale = (NumeroChilometri*prezzoAlChilometro*0.8);
+   discount = 'hai ricevuto il 20% di sconto!';
+   
+  
+
+
+  }else if(etaPasseggero.value =='Over65' ){
+   prezzoTotale = (NumeroChilometri*prezzoAlChilometro*0.6)
+   discount = 'hai ricevuto il 40% di sconto!';
+   
+ 
+  
+  }else{
+   prezzoTotale = (NumeroChilometri*prezzoAlChilometro);
+   discount = 'nessuno sconto applicato';
+  
+
+ 
+
+ 
+  }
+
+  console.log(prezzoTotale);
+
+  ticket.classList.toggle('d-none')
+
+  
+  document.getElementById('output').innerHTML ='Ciao! ' + Nome + ' ecco il tuo ticket dà ' + prezzoTotale.toFixed(2) + '€ ' + discount;
+
+
+
+  
   
   
 })
@@ -32,14 +72,5 @@ btn1.addEventListener('click',function(event){
 
 
 
-if(EtàPasseggero === 'Minorenne'){
-  prezzoTotale= (NumeroChilometri*prezzoAlChilometro*0.8)
-}else if(EtàPasseggero === 'Over65' ){
-  prezzoTotale= (NumeroChilometri*prezzoAlChilometro*0.6)
-}else {
-  prezzoTotale= (NumeroChilometri*prezzoAlChilometro)
-}
 
-console.log(prezzoTotale);
 
-document.getElementById('output').innerHTML +=  prezzoTotale.toFixed(2) + '€';
